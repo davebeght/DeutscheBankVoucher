1681290b8aee7c12a858b917d942cdd538e9c786
jonasrothfuss <jonas.rothfuss@gmx.de>
2017-03-18 10:43:53 +0100

    User model + database migration

--- mysite/dbank_api/models.py
+++ mysite/dbank_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=30, null=True)
+  last_name = models.CharField(max_length=50, null=True)
+  email = models.CharField(max_length=50, null=True)
+  access_token = models.CharField(max_length=400, null=True)
+  token_expiration = models.DateTimeField(null=True)
