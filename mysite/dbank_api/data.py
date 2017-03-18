75e85d4cec3ed02f0c12bceb09d30ff873bc5a09
fabio.ferreira@student.kit.edu <f4bio.ferreira@gmail.com>
2017-03-18 21:36:54 +0100

    added categories

---
+++ mysite/dbank_api/data.py
@@ -0,0 +1,21 @@
+class Categories:
+  supermarket = {'supermarket': ["Aldi Nord", "Lidl", "REWE", "REWE City", "Edeka", "Netto Marken-Discount", "Penny", "Norma", "NETTO", "E-Aktiv", "Alnatura", "basic", "Erdkorn", "Kaiser's",
+                "Tegut", "Tengelmann", "Reichelt", "Markant", "V-Markt", "DM", "Mueller", "Aldi Sued", "Marktkauf"]}
+
+
+  travel = {'travel': ["TUI", "Thomas Cook", "DER Touristik", "FTI", "Aida Cruises", "Alltours", "Schauinsland Reisen", "ab-in-den-Urlaub.de",
+                          "Expedia", "HolidayCheck", "Opodo.de", "Last-minute.de", "trivago.de", "weg.de", "ltur.com", "hrs.de", "swoodoo.com", "travel24.com"]}
+
+  transportation = {'transportation': ["Deutsche Bahn AG", "flixbus", "blablacar", "uber"]}
+
+  shopping = {'shopping': ["Amazon", "zalando", "esprit.de", "bonprix.de"]}
+
+
+
+  allCategories = {'supermarket': ("Aldi Nord", "Lidl", "REWE", "REWE City", "Edeka", "Netto Marken-Discount", "Penny", "Norma", "NETTO", "E-Aktiv", "Alnatura", "basic", "Erdkorn", "Kaiser\'s",
+                "Tegut", "Tengelmann", "Reichelt", "Markant", "V-Markt", "DM", "Mueller", "Aldi Sued", "Marktkauf"),
+              'travel': ("TUI", "Thomas Cook", "DER Touristik", "FTI", "Aida Cruises", "Alltours", "Schauinsland Reisen", "ab-in-den-Urlaub.de",
+                          "Expedia", "HolidayCheck", "Opodo.de", "Last-minute.de", "trivago.de", "weg.de", "ltur.com", "hrs.de", "swoodoo.com", "travel24.com"),
+              'transportation': ("Deutsche Bahn AG", "flixbus", "blablacar", "uber"),
+              'shopping': ("Amazon", "zalando", "esprit.de", "bonprix.de")}
+
